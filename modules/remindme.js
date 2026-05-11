7dbf1e0b85ae9e1b4a083fc047b1edd67255bb42
lemonnik <lemongamer6484@gmail.com>
2026-05-11 22:21:49 +0200

    fix database path

--- modules/remindme.js
+++ modules/remindme.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'reminders.db'));
+const db = new Database(path.join(__dirname, '../module_data/remindme/reminders.db'));
 
 db.exec(`
     CREATE TABLE IF NOT EXISTS reminders (
